f3896af46a957095757af0ea10e992bbe14e55e7
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2019-04-07 19:25:54 -0700

    rocks lol

--- source/Rock.hx
+++ source/Rock.hx
@@ -1,5 +1,6 @@
 package;
 
+import flixel.FlxG;
 import flixel.system.FlxAssets.FlxGraphicAsset;
 
 /**
@@ -12,6 +13,13 @@ class Rock extends Interactable
 	public function new(?X:Float=0, ?Y:Float=0, ?SimpleGraphic:FlxGraphicAsset) 
 	{
 		super(X, Y, SimpleGraphic);
+		
+		
+		loadGraphic("assets/images/gameArt/rock" + FlxG.random.int(1, 3) +".png");
+		immovable = true;
+		
+		offset.y = height - 16;
+		height = 16;
 		
 	}
 	
